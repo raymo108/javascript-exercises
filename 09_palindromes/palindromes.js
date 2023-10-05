2208e63c1a348490315edcacb1d3d9eae518204b
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2023-10-04 23:07:54 -0400

    Write function to pass palindromes.spec.js tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase. Any matches in str are replaced with an "empty"
+    // str, essentially removing that value
+    const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
 
+    return cleanStr === cleanStr.split('').reverse().join('');
 };
 
-// Do not edit below this line
+
 module.exports = palindromes;
