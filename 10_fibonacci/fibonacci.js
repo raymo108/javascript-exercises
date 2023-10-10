d7745c39d10a10747f9c8f77fe2d18446c39dced
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2023-10-10 19:01:17 -0400

    Write function to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let nInt = parseInt(n);
+    let prev = 0;
+    let curr = 1;
+    if (nInt > 0) {
 
+        for (let i = 2; i <= nInt; i++) {
+            let next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+
+        return curr;
+
+    }   else {
+        return 'OOPS';
+    }
+ 
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
