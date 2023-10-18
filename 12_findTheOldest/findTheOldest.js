45b25263ff4bff77f6ea10a960ea13230161171e
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2023-10-17 21:38:44 -0400

    Write function to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    arr.forEach(obj => {
+        obj.ageId = obj.yearOfDeath ? obj.yearOfDeath - obj.yearOfBirth : currentYear - obj.yearOfBirth;
+    });
+    
 
+    return maxAge = arr.reduce((maxObj, currentObj) => {
+        return currentObj['ageId'] > maxObj['ageId'] ? currentObj : maxObj;
+        
+    });
 };
+
+        
+
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
